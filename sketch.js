377d3d5f623475f46a84c6b3b82db75d777cd476
duckscancode1267 <70921825+duckscancode1267@users.noreply.github.com>
2020-10-29 21:05:15 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,50 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+var dustbinObj
+var PaperObj
+var groundObj
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+  rectMode(CENTER);
+
+	engine = Engine.create();
+	world = engine.world;
+	dustbinObj=new dustbin(1200,650);
+	 paperObject=new Paper(200,450,40); 
+	 groundObject=new ground(width/2,670,width,20); 
+	 //Create a Ground 
+	 var render = Render.create({ element: document.body, engine: engine, options: { width: 1200, height: 700, wireframes: false } });
+
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  dustbinObj.display();
+  groundObj,display();
+  PaperObj.display();
+  drawSprites();
+ 
+}
+function keyPressed() { 
+	if (keyCode === UP_ARROW) { 
+		Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:85,y:-85});
+	 } 
+	}
+
+
+
